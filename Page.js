deebe36dc7f5f01e6cc0fb7bb0194ea6640bd62f
Tobias Buschor <tobias.buschor@shwups.ch>
2019-06-17 21:53:32 +0200

    Update Page.js

--- Page.js
+++ Page.js
@@ -27,7 +27,7 @@ const Page = class extends Row {
             await row.makeIfNot();
             let text_id = await row.$text_id;
             if (!text_id) {
-                console.log('generate2')
+                console.log('generate 3')
                 const textPro = await TextPro.generate(this.db);
                 row.$text_id = text_id = textPro.id;
             }
